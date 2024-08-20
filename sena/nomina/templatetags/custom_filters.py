8492ad053af43133197df0535cd2ccc38c3df386
albertmota28092004 <albamota@soy.sena.edu.co>
2024-08-20 00:21:49 -0500

    commit todo

--- sena/nomina/templatetags/custom_filters.py
+++ sena/nomina/templatetags/custom_filters.py
@@ -8,3 +8,16 @@ def round_number(value, decimal_places):
         return f"{value:.{decimal_places}f}"
     except (ValueError, TypeError):
         return value
+
+@register.filter
+def remove_duplicates(value_list):
+    """
+    Remove duplicate items from a list.
+
+    Args:
+        value_list (list): The list from which to remove duplicates.
+
+    Returns:
+        list: A new list without duplicates.
+    """
+    return list(dict.fromkeys(value_list))
